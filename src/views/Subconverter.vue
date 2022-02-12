4e4fa46eadbfe2418431a5165b619a427f33cf65
sunyou726 <31587214+sunyou726@users.noreply.github.com>
2022-02-13 00:35:40 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
           "useful-sub.vercel.app": "https://useful-sub.vercel.app/sub?",
-	     "sub-beta.vercel.app": "https://sub-beta.vercel.app/sub?",
+          "sub-beta.vercel.app": "https://sub-beta.vercel.app/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
@@ -264,7 +264,7 @@ export default {
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-	     { value: "https://useful-sub.vercel.app/sub?" },
+          { value: "https://useful-sub.vercel.app/sub?" },
           { value: "https://sub-beta.now.sh/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
